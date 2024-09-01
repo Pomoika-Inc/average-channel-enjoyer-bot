6a84134d25171cfa270f27f2218021337ea3f1d8
Mr-Dm1try <mr.dmitriy666@gmail.com>
2024-09-01 23:55:14 +0200

    sessions

--- src/bot/middlewares/session.ts
+++ src/bot/middlewares/session.ts
@@ -1,12 +1,85 @@
-import { type Middleware, type SessionOptions, session as createSession } from 'grammy'
-import type { Context, SessionData } from '#root/bot/context.js'
+import { MemorySessionStorage, type Middleware, session as createSession } from 'grammy'
+import type { Context } from '#root/bot/context.js'
 
-type Options = Pick<SessionOptions<SessionData, Context>, 'getSessionKey' | 'storage'>
+// type Options = Pick<SessionOptions<SessionData, Context>, 'getSessionKey' | 'storage'>
 
-export function session(options: Options): Middleware<Context> {
+export interface SessionData {
+  user?: UserSessionData
+  userInGroup?: UserInGroupSessionData
+  group?: GroupSessionData
+}
+
+export interface UserSessionData {
+  login: string
+  walletAddress?: string
+  active: boolean
+}
+
+export interface UserInGroupSessionData {
+  admin: boolean
+  presavedJettons: number
+  active: boolean
+}
+
+export interface GroupSessionData {
+  login?: string
+  channel: number
+  adminReactionRatio: number
+  badReactions: string[]
+  active: boolean
+}
+
+export function getUserSessionKey(ctx: Omit<Context, 'session'>) {
+  return ctx.from?.id.toString()
+}
+
+export function getUserInGroupSessionKey(ctx: Omit<Context, 'session'>) {
+  return `${ctx.from?.id.toString()}/${ctx.chat?.id.toString()}`
+}
+
+export function getGroupSessionKey(ctx: Omit<Context, 'session'>) {
+  return ctx.chat?.id.toString()
+}
+
+export function session(): Middleware<Context> {
   return createSession({
-    getSessionKey: options.getSessionKey,
-    storage: options.storage,
-    initial: () => ({}),
+    type: 'multi',
+    user: {
+      getSessionKey: getUserSessionKey,
+      storage: new MemorySessionStorage<UserSessionData | undefined>(),
+    },
+    userInGroup: {
+      getSessionKey: getUserInGroupSessionKey,
+      storage: new MemorySessionStorage<UserInGroupSessionData | undefined>(),
+    },
+    group: {
+      getSessionKey: getGroupSessionKey,
+      storage: new MemorySessionStorage<GroupSessionData | undefined>(),
+    },
   })
+}
+
+export function initSession(): Middleware<Context> {
+  return (ctx) => {
+    if (!ctx.from?.is_bot) {
+      ctx.session.user = {
+        login: ctx.from?.username ?? '',
+        active: true,
+      }
+    }
+
+    if (ctx.message?.reply_to_message?.sender_chat?.type === 'channel') {
+      ctx.session.userInGroup = {
+        admin: false,
+        presavedJettons: 0,
+        active: true,
+      }
+      ctx.session.group = {
+        channel: ctx.message.reply_to_message.sender_chat.id,
+        adminReactionRatio: 0,
+        badReactions: [],
+        active: true,
+      }
+    }
+  }
 }
